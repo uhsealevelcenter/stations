a2939547c887068ce4c08d5333a8fc650d36a125
derek young <dyoung@noio.soest.hawaii.edu>
2018-07-12 19:02:22 -1000

    fix indentation

--- datepicker.js
+++ datepicker.js
@@ -1,32 +1,32 @@
 
-    $('#datepicker').datepicker({
-      format: "mm/yyyy",
-      // startDate: "01/2017",
-      // endDate: "02/2020",
-      startView: 1,
-      minViewMode: 1,
-      maxViewMode: 2,
-      todayHighlight: true
-    });
+$('#datepicker').datepicker({
+  format: "mm/yyyy",
+  // startDate: "01/2017",
+  // endDate: "02/2020",
+  startView: 1,
+  minViewMode: 1,
+  maxViewMode: 2,
+  todayHighlight: true
+});
 
-    $('#datepicker').on('changeDate', function() {
-      var date = $('#datepicker').datepicker('getFormattedDate').split("/")
-      var formattedDate = date[1] + date[0];
-      loadTide(stn, formattedDate);
-    });
+$('#datepicker').on('changeDate', function() {
+  var date = $('#datepicker').datepicker('getFormattedDate').split("/")
+  var formattedDate = date[1] + date[0];
+  loadTide(stn, formattedDate);
+});
 
-    function getCurrentDate() {
-      var d = new Date();
-      var m = ('0' + (d.getMonth() + 1).toString()).slice(-2);
-      var y = d.getFullYear().toString();
-      return y + m;
-    }
+function getCurrentDate() {
+  var d = new Date();
+  var m = ('0' + (d.getMonth() + 1).toString()).slice(-2);
+  var y = d.getFullYear().toString();
+  return y + m;
+}
 
-    function UrlExists(url) {
-      $.get(url)
-        .done(function() {
-          return false;
-        }).fail(function() {
-          return true;
-        })
-    }
+function UrlExists(url) {
+  $.get(url)
+  .done(function() {
+    return false;
+  }).fail(function() {
+    return true;
+  })
+}
