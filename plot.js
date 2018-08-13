392aa067393fd8eb6685c55e39b4f3785fe75600
Nemanja Komar <chunkymonkeykre8ive@gmail.com>
2018-08-13 12:17:44 -1000

    Add time zone conversion.

--- plot.js
+++ plot.js
@@ -10,7 +10,7 @@ function plotData(_stn) {
       //   console.log("Data for this station not found");
       //   return
       // }
-      function unpack(rows, key, unit, datum,ml,mh,lst) {
+      function unpack(rows, key, unit, datum, ml, mh, lst, tz) {
         if (err) {
           Plotly.purge("tideplot1");
           Plotly.purge("tideplot2");
@@ -24,18 +24,21 @@ function plotData(_stn) {
           if (row[key] == "")
             row[key] = "NaN";
           if (key === "Time") {
-            row[key]=row[key]+ ":00Z";
-            var date = new Date(row[key]);
-            date.setHours(date.getHours()+lst);
-            return date.toISOString();
-            // return row[key];
-
+            if (tz.innerHTML !== DEFAULT_VALUES.tzone) {
+              // The file that is being loaded is not in ISO format. Adding :00Z to hours for GMT converts to ISO so that we can manipualte the date later
+              // Don't add :00Z if it is already there
+              if (!row[key].includes(":"))
+                row[key] = row[key] + ":00Z";
+              var date = new Date(row[key]);
+              date.setHours(date.getHours() + lst);
+              return date.toISOString();
+            } else
+              return row[key];
           }
           // if seeking datum or time zone value return the value so the "else" portion is not executed
           if (key === "MHHW_NTDE" || key === "MLLW_NTDE" || key === "time_zone") {
             return row[key];
-          }
-          else {
+          } else {
             if (unit.innerHTML === DEFAULT_VALUES.unit && datum.innerHTML === DEFAULT_VALUES.datum) {
               //Do nothing
               // console.log("NO conversion on station change");
@@ -45,10 +48,10 @@ function plotData(_stn) {
               // console.log("Convert only units on station change");
               return row[key] * 0.0328084;
             } else if (unit.innerHTML === DEFAULT_VALUES.unit && datum.innerHTML != DEFAULT_VALUES.datum) {
-                  return (row[key]*1+(ml-mh));
+              return (row[key] * 1 + (ml - mh));
             } else {
               // console.log("Convert both units and datum on station change");
-                return (row[key]*1+(ml-mh)) * 0.0328084;
+              return (row[key] * 1 + (ml - mh)) * 0.0328084;
             }
           }
 
@@ -68,30 +71,32 @@ function plotData(_stn) {
       }
       var currentUnit = document.getElementById("unitToggle");
       var currentDatum = document.getElementById("datumToggle");
-      var unitYlabel = getLabels(currentUnit,currentDatum).unit;
-      var datumYlabel = getLabels(currentUnit,currentDatum).datum;
-      var yLabel1='Relative water level ('+unitYlabel+', '+datumYlabel+')';
-      var yLabel2='Relative water level ('+unitYlabel+')';
-
-
-      var MLLW = parseFloat (unpack(rows, 'MLLW_NTDE',currentUnit,currentDatum)[0]);
-      var MHHW = parseFloat(unpack(rows, 'MHHW_NTDE',currentUnit,currentDatum)[0]);
-      var time_zone =  parseFloat(unpack(rows, 'time_zone',currentUnit,currentDatum)[0]);
-
       var currentTZ = document.getElementById("timeToggle");
 
+      var unitYlabel = getLabels(currentUnit, currentDatum).unit;
+      var datumYlabel = getLabels(currentUnit, currentDatum).datum;
+      var yLabel1 = 'Relative water level (' + unitYlabel + ', ' + datumYlabel + ')';
+      var yLabel2 = 'Relative water level (' + unitYlabel + ')';
+
+
+      var MLLW = parseFloat(unpack(rows, 'MLLW_NTDE', currentUnit, currentDatum)[0]);
+      var MHHW = parseFloat(unpack(rows, 'MHHW_NTDE', currentUnit, currentDatum)[0]);
+      var LST = parseFloat(unpack(rows, 'time_zone', currentUnit, currentDatum)[0]);
+
+      var currentTZ = document.getElementById("timeToggle");
+      var xLabel = "Time/Date " + '(' + currentTZ.innerHTML + ')';
 
       // There is no need to unpack time vector for every tracer
       // because it is the same for each tracer
-      var timeVector = unpack(rows, 'Time', currentUnit, currentDatum, MLLW, MHHW, time_zone);
-      console.log(time_zone);
+      var timeVector = unpack(rows, 'Time', currentUnit, currentDatum, MLLW, MHHW, LST, currentTZ);
+      console.log(LST);
 
       var trace1 = {
         type: "scatter",
         mode: "lines",
         name: 'Tidal prediction',
         x: timeVector,
-        y: unpack(rows, 'Prediction', currentUnit, currentDatum, MLLW, MHHW, time_zone),
+        y: unpack(rows, 'Prediction', currentUnit, currentDatum, MLLW, MHHW, LST),
         line: {
           color: 'rgb(86, 180, 233)'
         }
@@ -99,13 +104,13 @@ function plotData(_stn) {
 
       // console.log("Time "+ unpack(rows, 'Time'));
       // console.log("type of time/date "+ typeof (unpack(rows, 'Time')[0]));
-      // console.log("T_ZONE "+ unpack(rows, 'time_zone')[0]);
+      // console.log("T_ZONE "+ unpack(rows, 'LST')[0]);
       var trace2 = {
         type: "scatter",
         mode: "lines",
         name: 'Tide gauge observation',
         x: timeVector,
-        y: unpack(rows, 'Observation', currentUnit, currentDatum, MLLW, MHHW, time_zone),
+        y: unpack(rows, 'Observation', currentUnit, currentDatum, MLLW, MHHW, LST),
         line: {
           color: 'rgb(213, 94, 0)'
         }
@@ -116,7 +121,7 @@ function plotData(_stn) {
         mode: "lines",
         name: 'Residual (observation minus tide)',
         x: timeVector,
-        y: unpack(rows, 'Residual', currentUnit, currentDatum, MLLW, MHHW, time_zone),
+        y: unpack(rows, 'Residual', currentUnit, currentDatum, MLLW, MHHW, LST),
         line: {
           color: 'rgb(0, 158, 115)'
         }
@@ -128,7 +133,7 @@ function plotData(_stn) {
         mode: "lines",
         name: 'Extreme low (5%)',
         x: timeVector,
-        y: unpack(rows, 'ExtremeLow', currentUnit, currentDatum, MLLW, MHHW, time_zone),
+        y: unpack(rows, 'ExtremeLow', currentUnit, currentDatum, MLLW, MHHW, LST),
         line: {
           color: 'rgb(0, 0, 0)',
           dash: 'dashdot'
@@ -140,7 +145,7 @@ function plotData(_stn) {
         mode: "lines",
         name: 'Extreme high (5%)',
         x: timeVector,
-        y: unpack(rows, 'ExtremeHigh', currentUnit, currentDatum, MLLW, MHHW, time_zone),
+        y: unpack(rows, 'ExtremeHigh', currentUnit, currentDatum, MLLW, MHHW, LST),
         line: {
           color: 'rgb(0, 0, 0)',
           dash: 'dashdot'
@@ -156,6 +161,7 @@ function plotData(_stn) {
         height: 450,
         autoresize: true,
         xaxis: {
+          title: xLabel,
           autorange: true,
           rangeselector: {
             buttons: [{
@@ -197,7 +203,7 @@ function plotData(_stn) {
         margin: {
           l: 70,
           r: 0, //105
-          b: 0,
+          b: 40,
           t: 80,
           pad: 3
         },
@@ -210,6 +216,7 @@ function plotData(_stn) {
         height: 450,
         autoresize: true,
         xaxis: {
+          title: xLabel,
           autorange: true,
           rangeselector: {
             buttons: [{
@@ -250,8 +257,8 @@ function plotData(_stn) {
         margin: {
           l: 70,
           r: 0, //105
-          b: 0,
-          t: 100,
+          b: 40,
+          t: 60,
           pad: 3
         },
       };
@@ -264,60 +271,57 @@ function plotData(_stn) {
         displayModeBar: false
       });
       $("#product_desc").show();
-      // Dropdwon menu station change behavior
-
-      // if ( currentUnit.innerHTML === DEFAULT_VALUES.unit && currentDatum.innerHTML === DEFAULT_VALUES.datum) {
-      //   //Do nothing
-      //   console.log("NO conversion on station change");
-      // } else if(currentUnit.innerHTML != DEFAULT_VALUES.unit && currentDatum.innerHTML === DEFAULT_VALUES.datum) {
-      //   //convert units to english with default datum
-      //   console.log("Convert only units on station change");
-      //   updatePlot();
-      // }else if (currentUnit.innerHTML === DEFAULT_VALUES.unit && currentDatum.innerHTML != DEFAULT_VALUES.datum) {
-      //   console.log("Convert only datum on station change");
-      // } else {
-      //   console.log("Convert both units and datum on station change");
-      //   updatePlot();
-      // }
 
       // On button click station behavior
-      $( "#unitToggle" ).off().on('click',function() {
+
+      $("#unitToggle").off().on('click', function() {
 
         if (currentUnit.innerHTML === "Metric") {
-        $( "#unitToggle" ).attr('tooltip', "Change to Metric");
+          $("#unitToggle").attr('tooltip', "Change to Metric");
           currentUnit.innerHTML = "English";
         } else if (currentUnit.innerHTML === "English") {
           currentUnit.innerHTML = "Metric";
-          $( "#unitToggle" ).attr('tooltip', "Change to English");
+          $("#unitToggle").attr('tooltip', "Change to English");
         }
-          updatePlot(currentUnit,currentDatum);
-});
-
-      $('#datumToggle').off().on('click',function() {
-        // console.log(currentDatum.innerHTML);
-        if (currentDatum.innerHTML === "MLLW"){
-          currentDatum.innerHTML = "MHHW";
-          $( "#datumToggle" ).attr('tooltip', "Change to MLLW");
-        }
-        else {
-          currentDatum.innerHTML = "MLLW";
-          $( "#datumToggle" ).attr('tooltip', "Change to MHHW");
-        }
-        updatePlot(currentUnit,currentDatum);
+        updatePlotData(currentUnit, currentDatum);
       });
 
-      function updatePlot(unit, datum) {
+      $('#datumToggle').off().on('click', function() {
+        // console.log(currentDatum.innerHTML);
+        if (currentDatum.innerHTML === "MLLW") {
+          currentDatum.innerHTML = "MHHW";
+          $("#datumToggle").attr('tooltip', "Change to MLLW");
+        } else {
+          currentDatum.innerHTML = "MLLW";
+          $("#datumToggle").attr('tooltip', "Change to MHHW");
+        }
+        updatePlotData(currentUnit, currentDatum);
+      });
+
+      $('#timeToggle').off().on('click', function() {
+        // console.log(currentDatum.innerHTML);
+        if (currentTZ.innerHTML === "GMT") {
+          currentTZ.innerHTML = "LST";
+          $("#timeToggle").attr('tooltip', "Change to GMT");
+        } else {
+          currentTZ.innerHTML = "GMT";
+          $("#timeToggle").attr('tooltip', "Change to LST");
+        }
+        updateTime(currentTZ);
+      });
+
+      function updatePlotData(unit, datum) {
         var columns = ["Prediction", "Observation", "Residual", "ExtremeLow", "ExtremeHigh"];
-        unitYlabel = getLabels(currentUnit,currentDatum).unit;
-        datumYlabel = getLabels(currentUnit,currentDatum).datum;
+        unitYlabel = getLabels(currentUnit, currentDatum).unit;
+        datumYlabel = getLabels(currentUnit, currentDatum).datum;
 
         for (var i = 0; i < columns.length; i++) {
           var update = {
-            y: [unpack(rows, columns[i], unit, datum, MLLW, MHHW, time_zone)]
+            y: [unpack(rows, columns[i], unit, datum, MLLW, MHHW, LST)]
           };
           var layout_update = {
             yaxis: {
-              title: 'Relative water level ('+unitYlabel+', '+datumYlabel+')',
+              title: 'Relative water level (' + unitYlabel + ', ' + datumYlabel + ')',
               autorange: true,
               range: [0, 1000],
               type: 'linear',
@@ -326,10 +330,48 @@ function plotData(_stn) {
           // layout_update.yaxis.title = "Relative water level (ft, MLLW)";
           Plotly.update('tideplot1', update, layout_update, [i]);
           if (columns[i] === "Residual") {
-            layout_update.yaxis.title = 'Relative water level ('+unitYlabel+')';
+            layout_update.yaxis.title = 'Relative water level (' + unitYlabel + ')';
             Plotly.update('tideplot2', update, layout_update, [0]);
           }
         }
+      }
+
+      function updateTime(tz) {
+        var xLabel = "Time/Date " + '(' + tz.innerHTML + ')';
+        var tv = {
+          x: [unpack(rows, 'Time', currentUnit, currentDatum, MLLW, MHHW, LST, tz)]
+        };
+        var update = {
+          xaxis: {
+            title: xLabel,
+            autorange: true,
+            rangeselector: {
+              buttons: [{
+                  count: 7,
+                  label: '1w',
+                  step: 'day',
+                  stepmode: 'backward'
+                },
+                {
+                  count: 1,
+                  label: '1m',
+                  step: 'month',
+                  stepmode: 'backward'
+                },
+                {
+                  step: 'all'
+                }
+              ],
+              x: 0,
+              y: 1.1
+
+            },
+            rangeslider: {},
+            type: 'date'
+          },
+        }
+        Plotly.update('tideplot1', tv, update);
+        Plotly.update('tideplot2', tv, update);
       }
     }
     // , function(error) {
@@ -341,41 +383,38 @@ function plotData(_stn) {
 
 };
 
-function getLabels(unit,datum){
-  var combo="";
+function getLabels(unit, datum) {
+  var combo = "";
   var unt = "cm";
   var dtm = "MLLW";
   if (unit.innerHTML === DEFAULT_VALUES.unit && datum.innerHTML === DEFAULT_VALUES.datum)
-    //Metric + MLLW
-      {
-        combo = "ML";
-        unt = "cm";
-        dtm = "MLLW";
-      }
-  else if (unit.innerHTML != DEFAULT_VALUES.unit && datum.innerHTML === DEFAULT_VALUES.datum)
-    //English units + MLLW
-      {
-        combo = "EL";
-        unt = "ft";
-        dtm = "MLLW";
-      }
-  else if (unit.innerHTML === DEFAULT_VALUES.unit && datum.innerHTML != DEFAULT_VALUES.datum)
-    //Metric + MHHW
-      {
-        combo = "MH";
-        unt = "cm";
-        dtm = "MHHW";
-      }
-  else
-    // English + MHHW
-      {
-        combo = "EH";
-        unt = "ft";
-        dtm = "MHHW";
-      }
+  //Metric + MLLW
+  {
+    combo = "ML";
+    unt = "cm";
+    dtm = "MLLW";
+  } else if (unit.innerHTML != DEFAULT_VALUES.unit && datum.innerHTML === DEFAULT_VALUES.datum)
+  //English units + MLLW
+  {
+    combo = "EL";
+    unt = "ft";
+    dtm = "MLLW";
+  } else if (unit.innerHTML === DEFAULT_VALUES.unit && datum.innerHTML != DEFAULT_VALUES.datum)
+  //Metric + MHHW
+  {
+    combo = "MH";
+    unt = "cm";
+    dtm = "MHHW";
+  } else
+  // English + MHHW
+  {
+    combo = "EH";
+    unt = "ft";
+    dtm = "MHHW";
+  }
   var result = {
-    "unit":unt,
-    "datum":dtm
+    "unit": unt,
+    "datum": dtm
   }
   return result;
 
